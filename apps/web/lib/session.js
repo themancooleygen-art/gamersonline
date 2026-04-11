463c3401e1c6ef6a5cc9748244423bec3c50a595
themancooleygen-art <themancooleygen@gmail.com>
2026-04-11 10:56:26 -0400

    Update session.js

--- apps/web/lib/session.js
+++ apps/web/lib/session.js
@@ -1,16 +1,1 @@
-import { SignJWT, jwtVerify } from "jose";
 
-const secret = new TextEncoder().encode(process.env.NEXTAUTH_SECRET);
-
-export async function createSessionToken(payload) {
-  return new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime("30d")
-    .sign(secret);
-}
-
-export async function verifySessionToken(token) {
-  const { payload } = await jwtVerify(token, secret);
-  return payload;
-}
